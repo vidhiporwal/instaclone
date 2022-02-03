db4babd10ab53d2ba25f6f7b356e7547dbc6f6e0
Vidhi porwal <55405011+vidhiporwal@users.noreply.github.com>
2022-02-03 20:55:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <img className="app_header_Image"
+        src="https://logos-world.net/imageup/Instagram/Instagram-Logo-PNG7.png" width="70" height="70"
+        alt=""
+        />
+      </div>
+      <h1>hello</h1>
+      
+    </div>
+  );
+}
+
+export default App;
